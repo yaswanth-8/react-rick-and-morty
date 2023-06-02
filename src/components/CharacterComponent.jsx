bae78cf979bde708a33faddeee9ea70819d3f6e9
yaswanth <ya8wanth@gmail.com>
2023-06-02 16:34:02 +0530

    add rick and morty project

--- src/components/CharacterComponent.jsx
+++ src/components/CharacterComponent.jsx
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
+import "./Character.css";
 import IndividualComponent from "./CharacterComponent/IndividualComponent";
 
 function CharacterComponent() {
-  const fetchData = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+  const [page, setPage] = useState(1);
+  const fetchData = async ({ queryKey }) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?page=${queryKey[1]}`
+    );
     const data = await response.json();
-    return data.results;
+    return data;
   };
-  const { data: characters, status } = useQuery("myData", fetchData);
+  const {
+    data: characters,
+    isPreviousData,
+    isLoading,
+    isError,
+  } = useQuery(["myData", page], fetchData, { keepPreviousData: true });
 
-  if (status === "loading") {
+  if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  if (status === "error") {
+  if (isError) {
     return <p>Loading</p>;
   }
 
   return (
     <div>
-      {characters &&
-        characters.map((character) => (
+      <h1>Rick and Morty</h1>
+      {characters.results &&
+        characters.results.map((character) => (
           <IndividualComponent key={character.id} character={character} />
         ))}
+      <div className="buttons">
+        <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+          👈🏼
+        </button>
+        <button
+          disabled={isPreviousData || !characters.info.next}
+          onClick={() => setPage(page + 1)}
+        >
+          👉🏼
+        </button>
+      </div>
     </div>
   );
 }
